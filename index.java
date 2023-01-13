2e76d29a5385e05e1f8e87bb95d60a0985edeb2b
DieZeitungsente <younes.haika@gmail.com>
2023-01-13 18:34:57 +0100

    testing

--- index.java
+++ index.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
 
 class index {
-    public static void main(String[] args) throws IOException{
-        String[] array = { "i am an array" };
+    public static void main(String[] args) throws IOException {
+        String[] array = {"value 1", "value 2"};
         db.add(array);
         String[][] database = db.inject();
-        system.printArrayln(database[db.find("i am an array")]);
+        system.printArrayln(database[5]);
     }
 }
