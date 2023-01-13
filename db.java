a03653a7cde01805aa684fbed8e7d1d988ccebf0
DieZeitungsente <younes.haika@gmail.com>
2023-01-13 12:57:53 +0100

    make it possible to write + other changes

--- db.java
+++ db.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class db {
-    public static String[][] injectdb() throws IOException{
+    public static String[][] inject() throws IOException{
         String[][] arr = new String[400][400];
         FileReader myReader = new FileReader("database.txt");
         BufferedReader myBuffer = new BufferedReader(myReader);
@@ -22,8 +24,8 @@ public class db {
         return arr;
     }
     public static int find(String item) throws IOException {
-        String newItem = "\"" + item + "\"";
-        String[][] array = injectdb();
+        String newItem = "" + item + "";
+        String[][] array = inject();
         int ret = 0;
         boolean returned = false;
         for(int x = 0;x < array.length;x ++){
@@ -38,6 +40,32 @@ public class db {
             return ret;
         } else {
             return 404_404_404;
+        }
+    }
+    public static void write(String[][] arr) {
+        try {
+            FileWriter myWriter = new FileWriter("database.txt", false);
+            BufferedWriter myBuffer = new BufferedWriter(myWriter);
+            myBuffer.write("{ ");
+            for (int i = 0; i < arr.length; i++) {
+                myBuffer.write("{ ");
+                for (int j = 0; j < arr[i].length; j++) {
+                    if(arr[i][j] != null && !arr[i][j].isEmpty()){
+                        myBuffer.write(arr[i][j]);
+                    } else {
+                        myBuffer.write(" ");
+                    }
+                    if(j != arr[i].length -1)
+                        myBuffer.write(", ");
+                }
+                if(i != arr[i].length - 1){
+                    myBuffer.write(" }, ");
+                }
+            }
+            myBuffer.write(" } }");
+            myBuffer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- index.java
+++ index.java
@@ -2,9 +2,10 @@ import java.io.IOException;
 
 class index {
     public static void main(String[] args) throws IOException{
-        String[][] array = db.injectdb();
-        System.out.println(db.find("s"));
-        int testing = 1; 
-        system.printArrayln(array[db.find(testing + "")]);
+        String[][] array = { {"test", "test"}, {"test", "test"} };
+        db.write(array);
+        String[][] database = db.inject();
+        system.printArrayln(database[1]);
+        system.printArrayln(database[db.find("test")]);
     }
 }
